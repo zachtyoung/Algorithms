43d2fc09e7dcc52bfeae995e3ff69b53e9c113e3
zachtyoung <zachyoung@Zachs-MacBook-Pro.local>
2020-01-22 18:57:44 -0600

    Sanity Commit

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,7 +3,12 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+    greatest = None
+    for i in range(0, len(prices)):
+      for j in range(i + 1, len(prices)):
+        if greatest is None or prices[j] - prices[i] > greatest:
+                greatest = prices[j] - prices[i]
+    return greatest
 
 
 if __name__ == '__main__':
@@ -13,3 +18,4 @@ if __name__ == '__main__':
   args = parser.parse_args()
 
   print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+
